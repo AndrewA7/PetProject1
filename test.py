f768fdd80cfd93a4986eb47db3c8fc298e839544
AndrewA7 <andrew.dzhulay@gmail.com>
2023-09-20 22:43:16 +0300

    ain
    tests added

--- test.py
+++ test.py
@@ -1,10 +1,17 @@
 import unittest
 
+from triangle_area import triangle_area
+
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_zero_side(self):
+        assert triangle_area(4, 4, 0) == "side can't be 0"
 
+    def test_wrong_side(self):
+        assert triangle_area(2, 3, 34) == "Incorrect triangle"
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_int_values(self):
+        assert triangle_area(4, 6, 9) == 9.562
+
+    def test_float_values(self):
+        assert triangle_area(4.9, 6.1, 3.8) == 9.305
--- triangle_area.py
+++ triangle_area.py
@@ -1,6 +1,14 @@
-from curses.ascii import isdigit
+import math
 
-a_side = input("side A = ")
-b_side = input("side B = ")
-c_side = input("side C = ")
 
+def triangle_area(a, b, c):
+    if a * b * c == 0:
+        answer = "side can't be 0"
+
+    elif a > b + c or b > a + c or c > a + b:
+        answer = "Incorrect triangle"
+
+    else:
+        answer = round((math.sqrt((a + b + c) * (b + c - a) * (a + c - b) * (a + b - c)) * 0.25), 3)
+    print(answer)
+    return answer
